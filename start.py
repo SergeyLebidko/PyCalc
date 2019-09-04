bd5bbee407607efa88ed46c5b6727105069a9e11
SergeyLebidko <it.sergeyler@mail.ru>
2019-09-04 12:06:35 +0300

    Добавлен код разбиения выражения на составные части

--- start.py
+++ start.py
@@ -1,5 +1,23 @@
-class BracketError(Exception):
-    pass
+# Типы элементов выражения
+OPEN_BRACKET = ['(']
+CLOSE_BRACKET = [')']
+OPERATIONS = list('+-*/^')
+NUMBERS = list('0123456789.')
+SYMBOLS = [chr(a) for a in range(ord('a'), ord('z') + 1)]
+
+
+# Класс для представления отдельных элементов выражения
+class Element:
+
+    def __init__(self, type_of_content, content):
+        self.type_of_content = type_of_content
+        self.content = content
+
+    def get_float_content(self):
+        return float(self.content)
+
+    def append_to_content(self, a):
+        self.content += a
 
 
 # Функция проверки скобок
@@ -11,19 +29,62 @@ def bracket_analysis(e):
         if c == ')':
             level -= 1
         if level < 0:
-            raise BracketError
+            raise Exception
     if level != 0:
-        raise BracketError
+        raise Exception
+
+
+# Функция, разбивающая выражение на отдельные элементы
+def parser(expr):
+    result = []
+    for char in expr:
+        if char in OPEN_BRACKET:
+            result.append(Element('open_bracket', char))
+            continue
+        if char in CLOSE_BRACKET:
+            result.append(Element('close_bracket', char))
+            continue
+        if char in OPERATIONS:
+            result.append(Element('operation', char))
+            continue
+        if char in NUMBERS:
+            if len(result) == 0:
+                result.append(Element('number', char))
+                continue
+            if result[-1].type_of_content == 'number':
+                result[-1].append_to_content(char)
+                continue
+            result.append(Element('number', char))
+            continue
+        if char in SYMBOLS:
+            if len(result) == 0:
+                result.append(Element('symbol', char))
+                continue
+            if result[-1].type_of_content == 'symbol':
+                result[-1].append_to_content(char)
+                continue
+            result.append(Element('symbol', char))
+            continue
+        raise Exception
+    return result
+
+
+# Функция, вычисляющая значение выражения
+def calculate(expr):
+    expr = '(' + expr + ')'
+    bracket_analysis(expr)
+    expr = parser(expr)
 
 
 # Начало выполнения программы
 while True:
-    expr = input('Введите выражение: ')
-    expr = expr.lower().replace(' ', '')
-    if expr == 'quit' or expr == 'q' or expr == 'exit' or expr == 'x':
+    e = input('Введите выражение: ')
+    e = e.lower().replace(' ', '')
+    if e == '':
+        continue
+    if e == 'quit' or e == 'q' or e == 'exit' or e == 'x':
         break
-    expr = '(' + expr + ')'
     try:
-        bracket_analysis(expr)
-    except BracketError:
+        calculate(e)
+    except Exception:
         print('Не удалось вычислить значение выражения')
